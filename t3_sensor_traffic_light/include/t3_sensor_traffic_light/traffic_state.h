a37474d2d6656ad56483e7969a70793357b04642
June Kim <godlyjune@gmail.com>
2023-01-26 15:49:27 +0900

    Change initial value for color

--- t3_sensor_traffic_light/include/t3_sensor_traffic_light/traffic_state.h
+++ t3_sensor_traffic_light/include/t3_sensor_traffic_light/traffic_state.h
@@ -37,9 +37,9 @@ public:
   // RED = 0; YELLOW = 1; GREEN = 2;
   int8_t color;
 
-  TrafficLight() : height(0), width(0), square(0), color(-1){};
+  TrafficLight() : height(0), width(0), square(0), color(0){};
   TrafficLight(const t3_msgs::BoundingBox& bbox)
-    : bounding_box(bbox), height(bbox.ymax - bbox.ymin), width(bbox.xmax - bbox.xmin), color(-1)
+    : bounding_box(bbox), height(bbox.ymax - bbox.ymin), width(bbox.xmax - bbox.xmin), color(0)
   {
     square = height * width;
   };
