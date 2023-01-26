1623001b08f69ee73b598533135ab45281e10bab
Kim Dongmin <reelteme@gmail.com>
2023-01-26 11:12:14 +0900

    Do_xycar_play_test

--- t3_sensor_stop_line/src/t3_sensor_stop_line.cpp
+++ t3_sensor_stop_line/src/t3_sensor_stop_line.cpp
@@ -12,16 +12,17 @@
 
 namespace color
 {
-static constexpr cv::Scalar WHITE = cv::Scalar(255, 255, 255);
-static constexpr cv::Scalar BLACK = cv::Scalar(0, 0, 0);
-static constexpr cv::Scalar RED = cv::Scalar(0, 0, 255);
-static constexpr cv::Scalar GREEN = cv::Scalar(0, 255, 0);
-static constexpr cv::Scalar BLUE = cv::Scalar(255, 0, 0);
-static constexpr cv::Scalar YELLOW = cv::Scalar(0, 255, 255);
+const cv::Scalar WHITE = cv::Scalar(255, 255, 255);
+const cv::Scalar BLACK = cv::Scalar(0, 0, 0);
+const cv::Scalar RED = cv::Scalar(0, 0, 255);
+const cv::Scalar GREEN = cv::Scalar(0, 255, 0);
+const cv::Scalar BLUE = cv::Scalar(255, 0, 0);
+const cv::Scalar YELLOW = cv::Scalar(0, 255, 255);
 }  // namespace color
 
 namespace sensor
 {
+
 const std::string NAME = "stop_line";
 const std::string SUB_TOPIC = "usb_cam/image_raw";
 const std::string PUB_TOPIC = "stop_line_data";
@@ -31,16 +32,22 @@ constexpr int HEIGHT = 480;
 
 class StopLine
 {
+  ros::NodeHandle node;
+  ros::Subscriber sub;
+  ros::Publisher pub;
   bool detected;
   int count;
+  cv::Mat vFrame;
+
 
 public:
   bool enable_debug;
+  t3_msgs::stop_line_data msg;
 
   StopLine()
   {
     node.param<bool>("sensor_stop_line_enable_debug", enable_debug, true);
-    sub = node.subscribe(SUB_TOPIC, 1, &Sensor::callback, this);
+    sub = node.subscribe(SUB_TOPIC, 1, &StopLine::callback, this);
     pub = node.advertise<t3_msgs::stop_line_data>(PUB_TOPIC, 1);
     if (enable_debug)
       cv::namedWindow(NAME);
@@ -67,16 +74,12 @@ void StopLine::callback(const sensor_msgs::ImageConstPtr& msg)
 
 void StopLine::publish()
 {
-  t3_msgs::stop_line_data msg;
-
   msg.header.stamp = ros::Time::now();
-  msg.header.frame_id = name;
-  msg.detected = detected;
-
+  msg.header.frame_id = NAME;
   pub.publish(msg);
-
+  
   if (enable_debug)
-    ROS_INFO("stop line detected: %s", detected ? "true" : "false");
+    ROS_INFO("stop line detected: %s", msg.detected ? "true" : "false");
 }
 
 void StopLine::process()
@@ -84,8 +87,9 @@ void StopLine::process()
   cv::Mat gray_image;
   cv::Mat blur_image;
   cv::Mat canny_image;
+  cv::cvtColor(this->vFrame, this->vFrame, cv::COLOR_BGR2RGB);
 
-  cv::cvtColor(this->vFrame,gray_image, cv::COLOR_BGT2GRAY);
+  cv::cvtColor(this->vFrame,gray_image, cv::COLOR_RGB2GRAY);
   cv::GaussianBlur(gray_image, blur_image, cv::Size(5, 5), 2);
   cv::Canny(blur_image, canny_image, 50, 150);
   cv::Mat roi = canny_image(cv::Rect(100, 370, 440, 50));
@@ -102,9 +106,15 @@ void StopLine::process()
     }
   }
   if (count >= 4)
-    this->publish();
+    msg.detected = true;
   else 
+    msg.detected = false;
+
+  this->publish();
+  //cv::imshow(NAME, this->vFrame);
+
 }
+
 }  // namespace sensor
 
 int main(int argc, char** argv)
@@ -116,7 +126,6 @@ int main(int argc, char** argv)
   while (ros::ok())
   {
     ros::spinOnce();
-
     if (stop_line.enable_debug)
     {
       int k = cv::waitKey(1);
@@ -124,7 +133,6 @@ int main(int argc, char** argv)
         break;
     }
   }
-
   cv::destroyAllWindows();
   return 0;
 }
