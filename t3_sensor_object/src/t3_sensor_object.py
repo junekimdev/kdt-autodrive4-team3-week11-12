2c1af60171af12af7f25da43d87849738b7167c0
June Kim <godlyjune@gmail.com>
2023-01-27 09:56:36 +0900

    Stop sending image bytes
    Image sending and receiving doesn't work for now

--- t3_sensor_object/src/t3_sensor_object.py
+++ t3_sensor_object/src/t3_sensor_object.py
@@ -129,10 +129,6 @@ class yolov3_trt(object):
             # if xycar_image is empty, skip inference
             if xycar_image.shape[0] == 0:
                 continue
-            
-            if self.show_img:
-                cv2.imshow(NAME, xycar_image)
-                cv2.waitKey(1)
 
             image = self.preprocessor.process(xycar_image)
             # Store the shape of the original input image in WH format, we will need it for later
@@ -179,29 +175,29 @@ class yolov3_trt(object):
         traffic_msg = traffic_light_image()
 
         # self._write_message(obj_msg, boxes, confs, classes)
-        if boxes is None:
-            return None
-        for box, score, category in zip(boxes, confs, classes):
-            # Populate darknet message
-            minx, miny, width, height = box
-            msg_bbox = BoundingBox()
-            msg_bbox.xmin = int(minx)
-            msg_bbox.xmax = int(minx + width)
-            msg_bbox.ymin = int(miny)
-            msg_bbox.ymax = int(miny + height)
-            msg_bbox.probability = score
-            msg_bbox.id = int(category)
-            if category != 5:
-                obj_msg.bounding_boxes.append(msg_bbox)
-            else:
-                traffic_msg.header.stamp = rospy.Time.now()
-                traffic_msg.header.frame_id = NAME+"traffic"
-                traffic_msg.bounding_box = msg_bbox
-                # image: NCHW format
-                traffic_msg.step = image.shape[1]
-                flat = image.reshape(1, image.shape[1]*image.shape[2]*image.shape[3])
-                traffic_msg.image_data = flat.tobytes() if image.data.contiguous else flat.copy().tobytes()
-                self.traffic_pub.publish(traffic_msg)
+        if boxes is not None:
+            for box, score, category in zip(boxes, confs, classes):
+                # Populate darknet message
+                minx, miny, width, height = box
+                msg_bbox = BoundingBox()
+                msg_bbox.xmin = int(minx)
+                msg_bbox.xmax = int(minx + width)
+                msg_bbox.ymin = int(miny)
+                msg_bbox.ymax = int(miny + height)
+                msg_bbox.probability = score
+                msg_bbox.id = int(category)
+                if category != 5:
+                    obj_msg.bounding_boxes.append(msg_bbox)
+                else:
+                    traffic_msg.header.stamp = rospy.Time.now()
+                    traffic_msg.header.frame_id = NAME+"traffic"
+                    traffic_msg.bounding_box = msg_bbox
+                    # img = np.array(np.transpose(image[0], (1,2,0)) * 255, dtype=np.uint8)
+                    # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+                    # traffic_msg.step = img.shape[1]*image.shape[2]
+                    # flat = img.reshape(1, -1)
+                    # traffic_msg.image_data = flat.tobytes() if image.data.contiguous else flat.copy().tobytes()
+                    self.traffic_pub.publish(traffic_msg)
         self.detection_pub.publish(obj_msg)
 
 
