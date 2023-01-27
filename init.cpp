a47968c0356425421c38696ff0992a2a79c65aec
DAHYUN KIM <dahyunkim@DAHYUNui-MacBookPro.local>
2023-01-27 10:34:30 +0900

    Traffic Init

--- init.cpp
+++ init.cpp
@@ -5,10 +5,20 @@ int start;
 start = 0;
 
 if (start == 0) {
-	if (sensor_state_.traffic_line.detected:) {
+	if (sensor_state_.traffic_line.detected) {
 		if (traffic_light.color == 2) {      //// Green ////
-			drive()
+			control_state_.reduce(mode, angle, 5)
 			start = 1
 		}
+	}
+}
+
+
+if (new_id == 5) {
+	if (sensor_state_.traffic_light.color == 2){
+		control_state_.reduce(mode, angle, 5)
+	}
+	else {
+		control_state_.reduce(mode, angle, 0)
 	}
 }
