6e1c9e49792ca0cb64f274ae16cb8778b43d30eb
DAHYUN KIM <dahyunkim@DAHYUNui-MacBookPro.local>
2023-01-27 10:23:54 +0900

    init

---
+++ init.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int start;
+
+start = 0;
+
+if (start == 0) {
+	if (sensor_state_.traffic_line.detected:) {
+		if (traffic_light.color == 2) {      //// Green ////
+			drive()
+			start = 1
+		}
+	}
+}
