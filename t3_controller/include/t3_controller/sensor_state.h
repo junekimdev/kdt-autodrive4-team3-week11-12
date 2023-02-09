beebe5d4458997cc42efa55603c7d630b1ae8b9d
tomy807 <tomy807@naver.com>
2023-02-09 20:57:39 +0900

    Init pingpong project

--- t3_controller/include/t3_controller/sensor_state.h
+++ t3_controller/include/t3_controller/sensor_state.h
@@ -4,10 +4,8 @@
 #include <vector>
 #include <numeric>
 
-#include "t3_msgs/lane_data.h"
+
 #include "t3_msgs/object_data.h"
-#include "t3_msgs/stop_line_data.h"
-#include "t3_msgs/traffic_light_data.h"
 
 namespace sensor_state
 {
@@ -16,54 +14,6 @@ constexpr int WIDTH = 640;
 constexpr int SMA_NUM = 10;
 
 // Define states
-struct Cam
-{
-  int width;
-  bool left_detected;
-  bool right_detected;
-  int lpos;
-  int rpos;
-  std::vector<int> lposMemo;
-  std::vector<int> rposMemo;
-  int lposSMA;
-  int rposSMA;
-
-  Cam()
-    : width(WIDTH)
-    , left_detected(false)
-    , right_detected(false)
-    , lpos(0)
-    , rpos(WIDTH - 1)
-    , lposMemo(std::vector<int>(SMA_NUM, 0))
-    , rposMemo(std::vector<int>(SMA_NUM, WIDTH - 1))
-    , lposSMA(0)
-    , rposSMA(WIDTH - 1)
-  {
-  }
-  void reduce(const t3_msgs::lane_data::ConstPtr& msg)
-  {
-    left_detected = msg->left_detected;
-    right_detected = msg->right_detected;
-    width = msg->width;
-    if (msg->left_detected)
-    {
-      lpos = msg->lpos;
-      lposMemo.erase(lposMemo.begin());
-      lposMemo.emplace_back(lpos);
-      lposSMA =
-          static_cast<int>(std::accumulate(lposMemo.begin(), lposMemo.end(), 0) / static_cast<float>(SMA_NUM) + .5f);
-    }
-    if (msg->right_detected)
-    {
-      rpos = msg->rpos;
-      rposMemo.erase(rposMemo.begin());
-      rposMemo.emplace_back(rpos);
-      rposSMA =
-          static_cast<int>(std::accumulate(rposMemo.begin(), rposMemo.end(), 0) / static_cast<float>(SMA_NUM) + .5f);
-    }
-  }
-};
-
 struct BoundingBox
 {
   int id;
@@ -103,59 +53,10 @@ struct Object
   };
 };
 
-struct StopLine
-{
-
-  bool detected;
-
-  StopLine()
-    : detected(false)
-  {};
-
-  void reduce(const t3_msgs::stop_line_data::ConstPtr& msg)
-  {
-    detected = msg->detected;
-  };
-};
-
-struct TrafficLight
-{
-
-  bool detected;
-  uint8_t color;
-  int count = 0;
-  BoundingBox boundingBox;
-  TrafficLight()
-    : color(-1),boundingBox(BoundingBox()),detected(false),count(0){};
-
-
-  void reduce(const t3_msgs::traffic_light_data::ConstPtr& msg)
-  {
-	if(msg->detected==false){
-		color = -1;
-		boundingBox = BoundingBox();
-		count = 0;
-	}else{
-		color = msg->color;
-		detected = msg->detected;
-		boundingBox.id = 5;
-		boundingBox.xmax = msg->bounding_box.xmax;
-		boundingBox.xmin = msg->bounding_box.xmin;
-		boundingBox.ymax = msg->bounding_box.ymax;
-		boundingBox.ymin = msg->bounding_box.ymin;
-		boundingBox.probability = msg->bounding_box.probability;
-	count = 1;
-	}
-  }
-};
-
 // Combine states
 struct State
 {
-  Cam cam;
   Object object;
-  StopLine stop_line;
-  TrafficLight traffic_light;
 };
 
 }  // namespace sensor
