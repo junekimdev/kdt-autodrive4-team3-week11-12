fbe69cbbce20dc96ab85d6f410eeb75a49237ab7
June Kim <godlyjune@gmail.com>
2023-02-13 21:15:43 +0900

    Refactor controller

--- t3_controller/include/t3_controller/control_state.h
+++ t3_controller/include/t3_controller/control_state.h
@@ -3,35 +3,80 @@
 
 #include <string>
 #include <vector>
+#include <memory>
+
+// #include "t3_controller/sensor_state.h"
+// #include "t3_controller/control_timer.h"
+#include "sensor_state.h"
+#include "control_timer.h"
 
 namespace control
 {
 
-constexpr int16_t ANGLE_CENTER = 0;
+constexpr int16_t ANGLE_CENTER = 5;
+constexpr int16_t ANGLE_MORE = 5;
 constexpr int16_t ANGLE_MIN = -50;
 constexpr int16_t ANGLE_MAX = 50;
+constexpr int16_t DRIVE_SPEED = 10;
+constexpr int64_t TIMER_IGNORE_MS = 10000LL;
 
-enum struct Mode
+class Mode
 {
-  Init,
-  Stop,
-  Go,
-  TurnLeft,
-  TurnRight
+  bool isStopGroupDetected(const sensor::State& sensor_state);
+  void setAngle(const sensor::State& sensor_state, int16_t center_pos);
+  void setAngleToFollowLeft(const sensor::State& sensor_state);
+  void setAngleToFollowRight(const sensor::State& sensor_state);
+  Mode transitFromInit(const sensor::State& sensor_state);
+  Mode transitFromReady(const sensor::State& sensor_state);
+  Mode transitFromLeft(const sensor::State& sensor_state);
+  Mode transitFromLeftPrepToStop(const sensor::State& sensor_state);
+  Mode transitFromLeftStop(const sensor::State& sensor_state);
+  Mode transitFromRight(const sensor::State& sensor_state);
+  Mode transitFromRightPrepToStop(const sensor::State& sensor_state);
+  Mode transitFromRightStop(const sensor::State& sensor_state);
+
+public:
+  enum struct Id
+  {
+    Init,
+    Ready,
+    Left,
+    LeftPrepToStop,
+    LeftStop,
+    Right,
+    RightPrepToStop,
+    RightStop,
+  };
+  Id id;
+  int16_t speed;
+  int16_t angle;
+  Timer timer;
+
+  Mode() : id(Id::Init), speed(0), angle(0)
+  {
+  }
+  Mode(Id id, int16_t speed, int16_t angle) : id(id), speed(speed), angle(angle)
+  {
+  }
+  Mode(Id id, Mode* mode) : id(id), speed(mode->speed), angle(mode->angle)
+  {
+  }
+
+  Mode next(const sensor::State& sensor_state);
 };
 
 struct State
 {
   Mode mode;
-  int16_t speed;
-  int16_t angle;
-  bool started;
 
-  State() : mode(Mode::Stop), speed(0), angle(ANGLE_CENTER), started(false)
+  State()
   {
   }
 
-  void reduce(){};
+  void reduce(const sensor::State& sensor_state)
+  {
+    mode = mode.next(sensor_state);
+  }
 };
 
 }  // namespace control
--- t3_controller/include/t3_controller/sensor_state.h
+++ t3_controller/include/t3_controller/sensor_state.h
@@ -2,7 +2,10 @@
 #define SENSOR_STATE_H
 
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 
+#include "t3_msgs/BoundingBox.h"
 #include "t3_msgs/lane_data.h"
 #include "t3_msgs/object_data.h"
 #include "t3_msgs/stop_line_data.h"
@@ -17,70 +20,189 @@ constexpr int SMA_NUM = 10;
 // Define states
 struct Cam
 {
+  bool online;
   int width;
-  bool leftDetected;
-  bool rightDetected;
+  bool left_detected;
+  bool right_detected;
   int lpos;
   int rpos;
-  std::vector<int> lposMemo;
-  std::vector<int> rposMemo;
-  int lposSMA;
-  int rposSMA;
+  std::vector<int> lpos_memo;
+  std::vector<int> rpos_memo;
+  int lpos_sma;
+  int rpos_sma;
+  int lane_width;
 
   Cam()
-    : width(WIDTH)
-    , leftDetected(false)
-    , rightDetected(false)
+    : online(false)
+    , width(WIDTH)
+    , left_detected(false)
+    , right_detected(false)
     , lpos(0)
     , rpos(WIDTH - 1)
-    , lposMemo(std::vector<int>(SMA_NUM, 0))
-    , rposMemo(std::vector<int>(SMA_NUM, WIDTH - 1))
-    , lposSMA(0)
-    , rposSMA(WIDTH - 1)
+    , lpos_memo(std::vector<int>(SMA_NUM, 0))
+    , rpos_memo(std::vector<int>(SMA_NUM, WIDTH - 1))
+    , lpos_sma(0)
+    , rpos_sma(WIDTH - 1)
+    , lane_width(WIDTH / 4)
   {
   }
   void reduce(const t3_msgs::lane_data::ConstPtr& msg)
   {
-    leftDetected = msg->leftDetected;
-    rightDetected = msg->rightDetected;
+    if (!online)
+      online = true;
+    left_detected = msg->left_detected;
+    right_detected = msg->right_detected;
     width = msg->width;
-    if (msg->leftDetected)
+    if (msg->left_detected)
     {
       lpos = msg->lpos;
-      lposMemo.erase(lposMemo.begin());
-      lposMemo.emplace_back(lpos);
-      lposSMA =
-          static_cast<int>(std::accumulate(lposMemo.begin(), lposMemo.end(), 0) / static_cast<float>(SMA_NUM) + .5f);
+      lpos_memo.erase(lpos_memo.begin());
+      lpos_memo.emplace_back(lpos);
+      lpos_sma =
+          static_cast<int>(std::accumulate(lpos_memo.begin(), lpos_memo.end(), 0) / static_cast<float>(SMA_NUM) + .5f);
     }
-    if (msg->rightDetected)
+    if (msg->right_detected)
     {
       rpos = msg->rpos;
-      rposMemo.erase(rposMemo.begin());
-      rposMemo.emplace_back(rpos);
-      rposSMA =
-          static_cast<int>(std::accumulate(rposMemo.begin(), rposMemo.end(), 0) / static_cast<float>(SMA_NUM) + .5f);
+      rpos_memo.erase(rpos_memo.begin());
+      rpos_memo.emplace_back(rpos);
+      rpos_sma =
+          static_cast<int>(std::accumulate(rpos_memo.begin(), rpos_memo.end(), 0) / static_cast<float>(SMA_NUM) + .5f);
     }
+    if (msg->left_detected && msg->right_detected)
+      lane_width = msg->rpos - msg->lpos;
   }
 };
 
-struct Object
+namespace traffic
 {
-  void reduce(const t3_msgs::object_data::ConstPtr& msg)
+
+enum struct Id
+{
+  None = -1,
+  Left = 0,
+  Right = 1,
+  Stop = 2,
+  Crosswalk = 3
+};
+
+struct Light
+{
+  enum struct Color
+  {
+    None = -1,
+    Red = 0,
+    Yellow = 1,
+    Green = 2
+  };
+
+  bool online;
+  bool detected;
+  Color color;
+  int area;
+
+  Light() : online(false), detected(false), color(Color::Red), distance(0.f)
   {
   }
+
+  void reduce(const t3_msgs::traffic_light_data::ConstPtr& msg)
+  {
+    if (!online)
+      online = true;
+    detected = msg->detected;
+    color = static_cast<Color>(msg->color);
+    // Traffic light is 2x larger than traffic signs
+    area = static_cast<int>((msg->bounding_box.xmax - msg->bounding_box.xmin) *
+                            (msg->bounding_box.ymax - msg->bounding_box.ymin));
+  }
 };
+
+struct Sign
+{
+  bool online;
+  bool left_detected;
+  bool right_detected;
+  bool stop_detected;
+  bool crosswalk_detected;
+  std::unordered_map<Id, int> area;
+  Id closest;
+
+  Sign()
+    : online(false)
+    , left_detected(false)
+    , right_detected(false)
+    , stop_detected(false)
+    , crosswalk_detected(false)
+    , area({ Id::Left, 0 }, { Id::Right, 0 }, { Id::Stop, 0 }, { Id::Crosswalk, 0 })
+    , closest(Id::None)
+  {
+  }
+
+  void reduce(const t3_msgs::object_data::ConstPtr& msg)
+  {
+    if (!online)
+      online = true;
+
+    // Reset
+    left_detected = false;
+    right_detected = false;
+    stop_detected = false;
+    crosswalk_detected = false;
+    for (const auto& id : area)
+      area[id] = 0;
+    closest = Id::None;
+
+    // Overwrite
+    if (sizeof(msg->bounding_boxes))
+    {
+      for (const auto& bbox : msg->bounding_boxes)
+      {
+        int bbox_area = static_cast<int>((bbox.bounding_box.xmax - bbox.bounding_box.xmin) *
+                                         (bbox.bounding_box.ymax - bbox.bounding_box.ymin));
+        switch (static_cast<Id>(bbox.id))
+        {
+          case Id::Left:
+            left_detected = true;
+            area[Id::Left] = bbox_area;
+            break;
+          case Id::Right:
+            right_detected = true;
+            area[Id::Right] = bbox_area;
+            break;
+          case Id::Stop:
+            stop_detected = true;
+            area[Id::Stop] = bbox_area;
+            break;
+          case Id::Crosswalk:
+            crosswalk_detected = true;
+            area[Id::Crosswalk] = bbox_area;
+            break;
+          default:
+            break;
+        }
+      }
+      const bool compare = [](const auto& a, const auto& b) { return a.second < b.second; };
+      max_pair = *std::max_element(area.begin(), area.end(), compare);
+      closest = max_pair.first;
+    }
+  }
+};
+}  // namespace traffic
 
 struct StopLine
 {
-  void reduce(const t3_msgs::stop_line_data::ConstPtr& msg)
+  bool online;
+  bool detected;
+
+  StopLine() : online(false), detected(false)
   {
   }
-};
 
-struct TrafficLight
-{
-  void reduce(const t3_msgs::traffic_light_data::ConstPtr& msg)
+  void reduce(const t3_msgs::stop_line_data::ConstPtr& msg)
   {
+    if (!online)
+      online = true;
+    detected = msg->detected;
   }
 };
 
@@ -88,9 +210,9 @@ struct TrafficLight
 struct State
 {
   Cam cam;
-  Object object;
   StopLine stop_line;
-  TrafficLight traffic_light;
+  traffic::Sign sign;
+  traffic::Light light;
 };
 
 }  // namespace sensor
